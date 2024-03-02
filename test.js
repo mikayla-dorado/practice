c167aa5dd5067b0bea3d6b5244d98393256b7e7e
mikayla dorado <mikayla.dorado17@gmail.com>
2024-03-01 22:46:52 -0600

    practice

--- test.js
+++ test.js
@@ -86,9 +86,6 @@ console.log(Check(13))
 
 
 
-
-
-
 // //! NEED TO MAKE A REACT PRACTICE FOLDER
 // //?React counter using useState
 
@@ -104,17 +101,6 @@ console.log(Check(13))
 //     )
 // } 
 
-for (let i = 1; i <= 100; i += 1) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz")
-    } else if (i % 3 === 0) {
-        console.log("Fizz")
-    } else if (i % 5 === 0) {
-        console.log("Buzz")
-    } else {
-        console.log(i)
-    }
-}
 
 //? this version is incorrect, the order of the if statements matter
 //?the third condition will never be reached because 'i % 3 === 0' was already met in the first condition, so it will skip 'FizzBuzz' and only print 'Fizz'
